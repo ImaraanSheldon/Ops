2aacb6cbd6631de2a7bb49d3ac02fbf86dd2fcba
ImaraanSheldon <imaraansheldonis2302003@gmail.com>
2024-05-16 16:30:47 +0200

    no

---
+++ script/code.js
@@ -0,0 +1,7 @@
+function insertInfo(){
+    let firstName = document.querySelector('[fName]').value;
+    let lastName = document.querySelector('[fName]').value;
+    let age = document.querySelector('[age]').value;
+    let age = document.querySelector('[dob]').value;
+    let age = document.querySelector('[gender]').value;
+}
